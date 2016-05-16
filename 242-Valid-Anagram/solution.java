d46c56bc834136a365e8ce62ba4eb7d4d4858ae3
Shiqi Zhong <szhong4@vols.utk.edu>
2016-05-16 13:25:30 -0400

    [242. Valid Anagram][Accepted]committed by LeetCode Extension

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -1,5 +1,18 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
-        
+        if (s == null && t == null) return true;
+            else if (s == null || t == null) return false;
+        if (s.length() != t.length()) return false;
+        int[] cache = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            cache[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            cache[t.charAt(i) - 'a']--;
+        }
+        for (int i : cache) {
+            if (i != 0) return false;
+        }
+        return true;
     }
 }
