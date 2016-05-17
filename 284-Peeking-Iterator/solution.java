376f66777f4b620ad76e3de617101eb396c82831
Shiqi Zhong <szhong4@vols.utk.edu>
2016-05-17 15:32:04 -0400

    [284. Peeking Iterator][Accepted]committed by LeetCode Extension

--- 284-Peeking-Iterator/solution.java
+++ 284-Peeking-Iterator/solution.java
@@ -1,26 +1,32 @@
 // Java Iterator interface reference:
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 class PeekingIterator implements Iterator<Integer> {
-
+    Iterator<Integer> peekItr;
+    Integer peek;
 	public PeekingIterator(Iterator<Integer> iterator) {
 	    // initialize any member here.
-	    
+	    peekItr = iterator;
+	    if (peekItr.hasNext()) {
+	        peek = peekItr.next();
+	    }
 	}
 
     // Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
-        
+        return peek;
 	}
 
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-	    
+	    int temp = peek;
+	    peek = peekItr.hasNext() ? peekItr.next():null;
+	    return temp;
 	}
 
 	@Override
 	public boolean hasNext() {
-	    
+	    return peek != null;
 	}
 }
