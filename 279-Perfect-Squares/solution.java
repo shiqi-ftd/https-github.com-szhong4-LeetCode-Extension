fb5efc704bf7046774c0e3b32e979c00ce144365
Shiqi Zhong <szhong4@vols.utk.edu>
2016-05-17 14:57:30 -0400

    [279. Perfect Squares][Accepted]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -1,5 +1,16 @@
 public class Solution {
     public int numSquares(int n) {
-        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int cur = Integer.MAX_VALUE;
+            int j = 1;
+            while (i - j * j >= 0) {
+                cur = Math.min(cur, dp[i - j * j] + 1);
+                j++;
+            }
+            dp[i] = cur;
+        }
+        return dp[n];
     }
 }
