fd77b5dcde403a97f53fe54ee1b4984955d36132
Shiqi Zhong <szhong4@vols.utk.edu>
2016-05-18 12:17:10 -0400

    [290. Word Pattern][Accepted]committed by LeetCode Extension

--- 290-Word-Pattern/solution.java
+++ 290-Word-Pattern/solution.java
@@ -3,14 +3,16 @@ public class Solution {
         if (pattern == null && str == null) return true;
         else if (pattern == null || str == null) return false;
         int pLen = pattern.length();
-        String[] word = str.split(' ');
+        String[] word = str.split(" ");
         int wLen = word.length;
-        if (plen != wlen) return false;
+        if (pLen != wLen) return false;
         
         HashMap<Character, String> hm = new HashMap();
         for (int i = 0; i < pLen; i++) {
             if (hm.containsKey(pattern.charAt(i))) {
                 if (!hm.get(pattern.charAt(i)).equals(word[i])) return false;
+            } else if (hm.containsValue(word[i])) {
+                return false;
             } else {
                 hm.put(pattern.charAt(i), word[i]);
             }
