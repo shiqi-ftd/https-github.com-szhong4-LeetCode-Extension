bd32d94842bae63f4846d0e252c9fefc66068212
Shiqi Zhong <szhong4@vols.utk.edu>
2016-05-18 12:31:05 -0400

    [299. Bulls and Cows][Accepted]committed by LeetCode Extension

---
+++ 299-Bulls-and-Cows/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0;
+        int cows = 0;
+        int[] numbers = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            int s = secret.charAt(i) - '0';
+            int g = guess.charAt(i) - '0';
+            if (s == g) bulls++;
+            else {
+                if (numbers[g] < 0) cows++;
+                if (numbers[s] > 0) cows++;
+                numbers[g] ++;
+                numbers[s] --;
+            }
+        }
+        return bulls + "A" + cows + "B";
+    }
+}
