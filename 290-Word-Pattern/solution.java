afdaff52c3843544a6abbfa9f726e86efc58d3d8
Shiqi Zhong <szhong4@vols.utk.edu>
2016-05-18 12:13:12 -0400

    [290. Word Pattern][Compile Error]committed by LeetCode Extension

--- 290-Word-Pattern/solution.java
+++ 290-Word-Pattern/solution.java
@@ -1,5 +1,20 @@
 public class Solution {
     public boolean wordPattern(String pattern, String str) {
+        if (pattern == null && str == null) return true;
+        else if (pattern == null || str == null) return false;
+        int pLen = pattern.length();
+        String[] word = str.split(' ');
+        int wLen = word.length;
+        if (plen != wlen) return false;
         
+        HashMap<Character, String> hm = new HashMap();
+        for (int i = 0; i < pLen; i++) {
+            if (hm.containsKey(pattern.charAt(i))) {
+                if (!hm.get(pattern.charAt(i)).equals(word[i])) return false;
+            } else {
+                hm.put(pattern.charAt(i), word[i]);
+            }
+        }
+        return true;
     }
 }
