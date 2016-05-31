c8f0fcebd3b41d5d56d2c94217f1b0dece1140fe
Shiqi Zhong <szhong4@vols.utk.edu>
2016-05-31 14:31:29 -0400

    [32. Longest Valid Parentheses][Time Limit Exceeded]committed by LeetCode Extension

--- 32-Longest-Valid-Parentheses/solution.java
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -1,5 +1,20 @@
 public class Solution {
     public int longestValidParentheses(String s) {
-        
+        if (s == null || s.length() < 2) return 0;
+        int counter = 0;
+        Stack<Character> stk = new Stack();
+        for (Character c : s.toCharArray()) {
+            if (c == '(') {
+                stk.push('(');
+            } else if (c == ')') {
+                if (stk.empty()) {
+                    continue;
+                } else {
+                    stk.pop();
+                    counter += 2;
+                }
+            }
+        }
+        return counter;
     }
 }
