90e6bf2fc4950807ab150ea97fcc7fc3c96cd5b4
Shiqi Zhong <szhong4@vols.utk.edu>
2016-05-31 14:44:42 -0400

    [32. Longest Valid Parentheses][Accepted]committed by LeetCode Extension

--- 32-Longest-Valid-Parentheses/solution.java
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int longestValidParentheses(String s) {        
-        Stack<Integer> stack = new Stack();
+        LinkedList<Integer> stack = new LinkedList<>();
         int result = 0;
         stack.push(-1);
         for (int i = 0; i < s.length(); i++) {
