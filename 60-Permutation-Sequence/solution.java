ea37d44f6f5d274a5d893adba98675bd0d632c5b
Shiqi Zhong <szhong4@vols.utk.edu>
2016-06-01 12:40:12 -0400

    [60. Permutation Sequence][Time Limit Exceeded]committed by LeetCode Extension

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,26 +1,31 @@
 public class Solution {
     List<Integer> kth;
+    int counter = 0;
     public String getPermutation(int n, int k) {
         int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
             nums[i] = i + 1;
         }
-        helper(k, 0, new ArrayList<Integer>(), nums);
+        helper(k, new ArrayList<Integer>(), nums);
         StringBuilder sb = new StringBuilder();
         for (int i : kth) {
             sb.append(i);
         }
         return sb.toString();
     }
-    public void helper(int end, int counter, ArrayList<Integer> cur, int[] nums) {
-        if (counter == end) {
-            kth = cur;
+    public void helper(int end, ArrayList<Integer> cur, int[] nums) {
+        if (cur.size() == nums.length) {
+            counter += 1;
+            if (counter == end) {
+                kth = new ArrayList(cur);
+                return;
+            }  
             return;
         }
         for (int i = 0; i < nums.length; i++) {
             if (cur.contains(nums[i])) continue;
             cur.add(nums[i]);
-            helper(end, counter + 1, cur, nums);
+            helper(end, cur, nums);
             cur.remove(cur.size() - 1);
         }
     }
