8fbbd79666d3ad3d803ed58dbb94bf773d13c3fe
Shiqi Zhong <szhong4@vols.utk.edu>
2016-06-01 12:27:13 -0400

    [60. Permutation Sequence][Runtime Error]committed by LeetCode Extension

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,29 +1,27 @@
 public class Solution {
+    List<Integer> kth;
     public String getPermutation(int n, int k) {
         int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
             nums[i] = i + 1;
         }
-        List<List<Integer>> res = new ArrayList();
-        helper(k, res, new ArrayList<Integer>(), nums);
-        List<Integer> kth = res.get(k - 1);
+        helper(k, 0, new ArrayList<Integer>(), nums);
         StringBuilder sb = new StringBuilder();
         for (int i : kth) {
             sb.append(i);
         }
         return sb.toString();
     }
-    public void helper(int end, List<List<Integer>> res, ArrayList<Integer> cur, int[] nums) {
-        if (cur.size() == nums.length) {
-            res.add(new ArrayList(cur));
+    public void helper(int end, int counter, ArrayList<Integer> cur, int[] nums) {
+        if (counter == end) {
+            kth = cur;
             return;
         }
         for (int i = 0; i < nums.length; i++) {
             if (cur.contains(nums[i])) continue;
             cur.add(nums[i]);
-            helper(end, res, cur, nums);
+            helper(end, counter + 1, cur, nums);
             cur.remove(cur.size() - 1);
-            if (res.size() == end) break;
         }
     }
 }
