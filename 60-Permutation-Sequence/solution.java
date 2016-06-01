6f327a6d8c8fc3eee3efc95e10ee05c821626b61
Shiqi Zhong <szhong4@vols.utk.edu>
2016-06-01 13:43:58 -0400

    [60. Permutation Sequence][Compile Error]committed by LeetCode Extension

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,31 +1,19 @@
-public class Solution {
-    List<Integer> kth;
-    int counter = 0;
-    public String getPermutation(int n, int k) {
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = i + 1;
-        }
-        helper(k, new ArrayList<Integer>(), nums);
-        StringBuilder sb = new StringBuilder();
-        for (int i : kth) {
-            sb.append(i);
-        }
-        return sb.toString();
+public class Solution {    
+    if (n < 1 || n > 9 || k < 1) return "";
+
+    int[] fac = new int[n + 1];
+    fac[0] = 1;
+    for (int i = 1; i < n; i++) fac[i] = i * fac[i - 1];
+
+    List<Integer> numbers = new ArrayList<>();
+    for (int i = 1; i <= n; i++) numbers.add(i);
+    
+    StringBuilder sb = new StringBuilder();
+    k-= 1;
+    for (int i = n - 1; i >= 0; i--) {
+        int index = k / fac[i];
+        sb.append(numbers.remove(index));
+        k %= fac[i];
     }
-    public void helper(int end, ArrayList<Integer> cur, int[] nums) {
-        if (cur.size() == nums.length) {
-            counter += 1;
-            if (counter == end) {
-                kth = cur;
-            }  
-            return;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (cur.contains(nums[i])) continue;
-            cur.add(nums[i]);
-            helper(end, cur, nums);
-            cur.remove(cur.size() - 1);
-        }
-    }
+    return sb.toString();
 }
