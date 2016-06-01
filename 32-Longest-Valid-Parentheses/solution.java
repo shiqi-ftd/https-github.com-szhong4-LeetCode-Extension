b5e7c757a8b0df2c2dca4fca22f696152c91d993
Shiqi Zhong <szhong4@vols.utk.edu>
2016-06-01 12:19:06 -0400

    [32. Longest Valid Parentheses][Time Limit Exceeded]committed by LeetCode Extension

--- 32-Longest-Valid-Parentheses/solution.java
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -1,15 +1,28 @@
 public class Solution {
-    public int longestValidParentheses(String s) {        
-        Stack<Integer> stack = new Stack();
-        int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ')' && stack.size() > 1 && s.charAt(stack.peek()) == '(') {
-                stack.pop();
-                result = Math.max(result, i - stack.peek());
-            } else {
-                stack.push(i);
+    public int longestValidParentheses(String s){
+        if (s==null||s.length()==0) return 0;
+
+        Stack<Integer> stack= new Stack<Integer>(); //Store indices of '('
+        int[] result=new int[s.length()];//Store the length of the current longest valid sequence.
+
+        int max=0;
+
+        for (int i=0;i<s.length();i++)
+            if (s.charAt(i)=='(')
+                stack.push(i);  
+
+            else if (s.charAt(i)==')'){
+                if (stack.isEmpty()) 
+                    continue;
+                else if (stack.peek()>0) 
+                    result[i]=2+result[stack.pop()-1]+result[i-1];//May connect two valid sequences, or increase the length of current valid sequence. 
+                else {
+                    result[i]=2+result[i-1];//Handle the special case that the leftmost char is a '('
+                    stack.pop();
+                }
+
+                max=Math.max(result[i],max);
             }
-        }
-        return result;
-}
+        return max;
+    }
 }
